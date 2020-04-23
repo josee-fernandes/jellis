63c76a66cc08943fe4b9b95e938edccc013f59c9
José Vitor <jose-vitor-7@hotmail.com>
2020-04-23 19:45:28 -0300

    Problema com dependências resolvido

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -18,7 +17,14 @@ const createWindow = () => {
     minWidth: 800,
     minHeight: 800,
     title: 'Recorder',
-    
+    autoHideMenuBar: true,
+    center: true,
+    backgroundColor: '#262626',
+    titleBarStyle: 'hidden',
+    trafficLightPosition: {
+      x : 12,
+      y : 12
+    }
   });
 
   // and load the index.html of the app.
