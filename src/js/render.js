4271bf735a6ffd5ddafa8b1f0f15a5d2e62b2e44
José Vitor <jose-vitor-7@hotmail.com>
2020-04-23 02:22:49 -0300

    Estilização parcial e referência de elementos no js

---
+++ src/js/render.js
@@ -0,0 +1,5 @@
+// Buttons
+const videoElement = document.querySelector('video');
+const btnStart = document.querySelector('.js-btn-start');
+const btnStop = document.querySelector('.js-btn-stop');
+const btnVideoSelect = document.querySelector('.js-btn-video-select');
